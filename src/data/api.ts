ecdcde70dc845352dea7065d755427c2be80abac
Kayquedias <kayquedias204@gmail.com>
2024-05-30 17:31:05 -0300

    add: api functions to communicate with localstorage

---
+++ src/data/api.ts
@@ -0,0 +1,20 @@
+export function saveToLocalStorage<T>(key: string, value: T): void {
+  try {
+    const serializedValue = JSON.stringify(value);
+    localStorage.setItem(key, serializedValue);
+  } catch (error) {
+    console.error(`Error saving to local storage: ${error}`);
+  }
+}
+
+export function getFromLocalStorage<T>(key: string): T | null {
+  try {
+    const serializedValue = localStorage.getItem(key);
+    if (!serializedValue) return null;
+
+    return JSON.parse(serializedValue) as T;
+  } catch (error) {
+    console.error(`Error retrieving from local storage: ${error}`);
+    return null;
+  }
+}
